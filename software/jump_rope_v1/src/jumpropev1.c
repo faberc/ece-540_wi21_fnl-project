5e3e56337a2b621a8d8b9d3641373765762a2358
bthorn2 <bthorn2@pdx.edu>
2021-02-24 13:44:28 -0800

    Basic rope movement added

--- software/jump_rope_v1/src/jumpropev1.c
+++ software/jump_rope_v1/src/jumpropev1.c
@@ -12,12 +12,16 @@
  */
 
 // #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // Nexys Port Addresses
 #define PORT_LEDS       0x80001404      // (o) LEDs
 #define PORT_SLSWTCH    0x80001400      // (i) slide switches
 #define PORT_PBTNS      0x80001500      // (i) pushbuttons inputs
 #define PORT_GPIO_EN    0x80001408      // (o) enable LEDs for output
+
+#define ROPE_REG        0x80001704
 
 #define PORT_SEVENSEG_EN    0x80001038      // (o) 7 Segment enable
 #define PORT_SEVENSEG_HGH   0x8000103c      // (o) 7 Segment Higher Display
@@ -38,14 +42,54 @@
 
 int main ( void ) 
 {
-    int En_Value=0xFFFF, switches_value;
+    int En_Value = 0xFFFF;    //GPIO enable value
+    int start = 0x00;
 
-    WRITE_GPIO(PORT_GPIO_EN, En_Value);
+    int vga_width = 768;
+    int half_vga_width = vga_width / 2;       // = 384    
+    int bpm = 150;                            // beats per minute -- defined by song
+    int mpb = 1 / bpm;                        // minutes per beat
+    int spb = mpb / 60.0;                     // seconds per beat
+    int hbd = spb / 2.0;                      // half-beat delay
+    
+    char jump_array[] = {0b00, 0b00, 0b00, 0b01, 0b00, 0b00, 0b00, 0b01, 0b00, 0b00};
+    int jump_array_size = sizeof jump_array;
+
+    WRITE_GPIO(PORT_GPIO_EN, En_Value);         // Enable GPIOs
+    WRITE_GPIO(ROPE_REG, half_vga_width);       // Start rope in the middle of the screen
+    
+    int j;
 
     while (1) { 
-        switches_value = READ_GPIO(PORT_SLSWTCH);
-        switches_value = switches_value >> 16;
-        WRITE_GPIO(PORT_LEDS, switches_value);
+
+        start = READ_GPIO(PORT_PBTNS);          // Game starts when the user presses any button
+
+        if(start) {
+            WRITE_GPIO(ROPE_REG, 10);
+            for(j=0;j<10000000;j++);
+
+            int i = 0;
+            
+            for(i = 0; i < jump_array_size; i++){
+                switch (jump_array[i])
+                {
+                case 0b00:  // Rope stays at center
+                    WRITE_GPIO(ROPE_REG, half_vga_width);
+                    break;
+                
+                case 0b01:  //Jump action -- rope moves down and back to center
+                    WRITE_GPIO(ROPE_REG, vga_width);
+                    break;
+
+                default:
+                    WRITE_GPIO(ROPE_REG, half_vga_width);
+                    break;
+                }
+                for(j=0;j<10000000;j++);
+            }
+        }
+
+
     }
 
     return(0);    
