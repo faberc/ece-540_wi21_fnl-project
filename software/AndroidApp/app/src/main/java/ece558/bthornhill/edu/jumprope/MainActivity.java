a81b2fef473389779b1e8dd9013ce499841a1eb1
bthorn2 <bthorn2@pdx.edu>
2021-03-03 15:05:42 -0800

    Redesigned app to use activities instead of fragments

--- software/AndroidApp/app/src/main/java/ece558/bthornhill/edu/jumprope/MainActivity.java
+++ software/AndroidApp/app/src/main/java/ece558/bthornhill/edu/jumprope/MainActivity.java
@@ -5,17 +5,22 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+
+    private Button mStartGameButton;
 
 
     @Override
@@ -23,15 +28,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        mStartGameButton = (Button) findViewById(R.id.button_play);
+        mStartGameButton.setOnClickListener(ButtonListener);
 
-        if(fragment == null){
-            fragment = new GameplayFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
 
     }
+
+    View.OnClickListener ButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent i = new Intent(MainActivity.this, GameplayActivity.class);
+
+            switch (v.getId()){
+                case R.id.button_play:
+                    startActivity(i);
+                    break;
+            }
+
+        }
+    };
+
 
 }
 
